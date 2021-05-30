9753752010aa4077f21f1ecf905958dfaaf7eaeb
Will Boland <s3656903@student.rmit.edu.au>
2021-05-30 21:46:57 +1000

    Updated sample Flask code
    Create a flask application with environment variables and deployed onto AWS EBS with no issues

--- application.py
+++ application.py
@@ -1,10 +1,21 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
 
 application = Flask(__name__)
 
+def user_subscriptions():
+    dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
+    table = dynamodb.Table('Subscription')
+    response = table.query(KeyConditionExpression=Key('email').eq("s36569030@student.rmit.edu.au"))
+    items = list(response['Items'])
+    return items
+    
+
 @application.route("/")
 def index():
-    return "hello, Welcome"
+    subs = user_subscriptions()
+    return render_template('index.html', subs=subs)
 
 
 if __name__ == '__main__':
